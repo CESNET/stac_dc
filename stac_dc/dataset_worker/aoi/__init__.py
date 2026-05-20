6b7d7b09e7424f946a8d0069c70c81a376dbd6f8
Jiří Matějka <jirimatejka@cesnet.cz>
2026-05-20 11:00:05 +0200

    slovakia aoi

--- stac_dc/dataset_worker/aoi/__init__.py
+++ stac_dc/dataset_worker/aoi/__init__.py
@@ -1,10 +1,12 @@
 from .aoi import AOI
 from .czech_republic_aoi import CzechRepublicAOI
 from .finland_aoi import FinlandAOI
+from .slovakia_aoi import SlovakiaAOI
 from .spain_aoi import SpainAOI
 
 aois_map: dict[str, type[AOI]] = {
     "czech-republic": CzechRepublicAOI,
     "finland": FinlandAOI,
     "spain": SpainAOI,
+    "slovakia": SlovakiaAOI
 }
